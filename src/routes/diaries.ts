0fd05093d29b40fd626654b3ce222f483ea91158
Nick Perry <nick.perry12@outlook.com>
2024-08-07 10:37:58 -0700

    update

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import diaryService from '../services/diaryService';
+import toNewDiaryEntry from '../utils';
 
 const router = express.Router();
 
@@ -19,16 +20,30 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-  const { date, weather, visibility, comment } = req.body;
-  const addedEntry = diaryService.addDiary({
-    date,
-    weather,
-    visibility,
-    comment
-  });
+  // /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+  // const { date, weather, visibility, comment } = req.body;
+  // const addedEntry = diaryService.addDiary({
+  //   date,
+  //   weather,
+  //   visibility,
+  //   comment
+  // });
 
-  res.status(201).json(addedEntry);
+  // res.status(201).json(addedEntry);
+
+  try {
+    const newDiaryEntry = toNewDiaryEntry(req.body);
+
+    const addedEntry = diaryService.addDiary(newDiaryEntry);
+    res.status(201).json(addedEntry);
+  } catch (error) {
+    let errorMessage: string = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
---
+++ src/utils.ts
@@ -0,0 +1,72 @@
+import { NewDiaryEntry } from './types';
+import { Weather } from './types';
+import { Visibility } from './types';
+
+const isVisibility = (param: string): param is Visibility => {
+  return Object.values(Visibility).map(val => val.toString()).includes(param);
+};
+
+const parseVisibility = (visibility: unknown): Visibility => {
+  if (!visibility || !isString(visibility) || !isVisibility(visibility)) {
+    throw new Error(`Incorrect or missing visibility: ${visibility}`);
+  }
+
+  return visibility;
+};
+
+const isWeather = (param: string): param is Weather => {
+  return Object.values(Weather).map(val => val.toString()).includes(param);
+};
+
+const parseWeather = (weather: unknown): Weather => {
+  if (!isString(weather) || !isWeather(weather)) {
+    throw new Error(`Incorrect or missing weather: ${weather}`);
+  }
+
+  return weather;
+};
+
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date));
+};
+
+const parseDate = (date: unknown): string => {
+  if (!isString(date) || !isDate(date)) {
+    throw new Error(`Incorrect or missing date: ${date}`);
+  }
+
+  return date;
+};
+
+const isString = (string: unknown): string is string => {
+  return typeof string === 'string' || string instanceof String;
+};
+
+const parseComment = (comment: unknown): string => {
+  if (!isString(comment)) {
+    throw new Error('Incorrect or missing comment');
+  }
+
+  return comment;
+};
+
+const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
+  if ( !object || typeof object !== 'object' ) {
+    throw new Error('Incorrect or missing data');
+  }
+
+  if ('comment' in object && 'date' in object && 'weather' in object && 'visibility' in object)  {
+    const newEntry: NewDiaryEntry = {
+      weather: parseWeather(object.weather),
+      visibility: parseVisibility(object.visibility),
+      date: parseDate(object.date),
+      comment: parseComment(object.comment)
+    };
+
+    return newEntry;
+  }
+
+  throw new Error('Incorrect data: some fields are missing');
+};
+
+export default toNewDiaryEntry;
